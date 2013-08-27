98f533990068a42f06f8bea9910e92a112c5ff3d
Martin Suchanek <mrtn@nrd.io>
2013-08-26 23:37:45 -0700

    Add nuspec for WebChauffeur.SpecFlow. Update assembly descriptions.

--- WebChauffeur.SpecFlow/Properties/AssemblyInfo.cs
+++ WebChauffeur.SpecFlow/Properties/AssemblyInfo.cs
@@ -2,5 +2,5 @@
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("WebChauffeur.SpecFlow")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("WebChauffeur.SpecFlow. SpecFlow porcelain for WebChauffeur.")]
 [assembly: Guid("55a2ee33-a791-4297-803d-2a01e4d13f5e")]
--- WebChauffeur/Properties/AssemblyInfo.cs
+++ WebChauffeur/Properties/AssemblyInfo.cs
@@ -2,5 +2,5 @@
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("WebChauffeur")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("WebChauffeur. PageObjects on top of Selenium WebDriver.")]
 [assembly: Guid("67b60d3d-cecb-4437-9552-132793f6120b")]
