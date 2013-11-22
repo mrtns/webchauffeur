e38245ac571c0f274980bdf30f55484fe80f50f5
Martin Suchanek <mrtn@nrd.io>
2013-11-22 14:31:42 -0800

    Separate PageObject steps from Element steps

--- WebChauffeur.SpecFlow/PageObjectSteps.cs
+++ WebChauffeur.SpecFlow/PageObjectSteps.cs
@@ -16,19 +16,22 @@ namespace WebChauffeur.SpecFlow
             GivenILoadThePage(pageName);
         }
 
-        [When(@"I click the ""([^""]*)""")]
-        public void WhenIClickTheElement(string elementName) {
-            Context.CurrentPage.FindElementByName(elementName).GetWebElement(Context.Driver).Click();
-        }
-
         [Then(@"I should see the ""([^""]*)"" page")]
         public void ThenIShouldSeeThePage(string pageName) {
             Assert.That(Context.Site.GetPage(pageName).VerifyThatBrowserIsOnPage(Context.Driver), Is.True);
         }
+    }
 
+    [Binding]
+    public class ElementSteps
+    {
         [Then(@"I should see the ""([^""]*)""")]
         public void ThenIShouldSeeThe(string elementName) {
             Assert.That(Context.CurrentPage.FindElementByName(elementName).GetWebElement(Context.Driver), Is.Not.Null);
+        }
+        [When(@"I click the ""([^""]*)""")]
+        public void WhenIClickTheElement(string elementName) {
+            Context.CurrentPage.FindElementByName(elementName).GetWebElement(Context.Driver).Click();
         }
     }
 }
